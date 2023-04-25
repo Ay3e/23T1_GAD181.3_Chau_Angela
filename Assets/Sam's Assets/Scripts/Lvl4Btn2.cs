96b5a5b0cdbda2093c79d0d4a3d9bfd39e3344c1
SamGman1 <88645353+SamGman1@users.noreply.github.com>
2023-04-25 13:44:34 +1000

    Added level 4
    Added a fourth level. Made some Minot changes to level 2.

--- Assets/Sam's Assets/Scripts/Lvl4Btn2.cs
+++ Assets/Sam's Assets/Scripts/Lvl4Btn2.cs
@@ -2,34 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonPress : MonoBehaviour
+public class Lvl4Btn2 : MonoBehaviour
 {
-
-    public GameObject WallBlock;
     public GameObject WallBlock2;
     public bool onButton = false;
-    public bool stage2Ready = false;
     
+
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
     {
         onButton = true;
-        stage2Ready = true;
+        
     }
-   
-
     void Update()
     {
-        if(onButton == true)
+        if (onButton == true)
         {
 
-            WallBlock.SetActive(false);
-        }
-        if(stage2Ready == true)
-        {
             WallBlock2.SetActive(false);
         }
-
     }
 }
-    
